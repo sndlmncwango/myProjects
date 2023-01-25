938dc33b3f2eccb7085374709ef0f78b7f2fa2c5
sndlmncwango <sndlmncwango@gmail.com>
2023-01-25 20:41:30 +0200

    function to move div up and down

--- index.js
+++ index.js
@@ -1,22 +1,37 @@
 const player = document.querySelector("#player");
-let leftRifgtMove = -580;
+let leftRifgtMove = 0;
 let upDownMove = 0;
 window.addEventListener("keydown",(e)=>{
     rightMove(e);
     leftMove(e);
+    upMove(e);
+    downMove(e);
 })
 
 function rightMove(e){
     if(e.key === "ArrowRight"){
         leftRifgtMove -= 20;
         player.style.right = `${leftRifgtMove}px`
-        console.log(leftRifgtMove)
     }
 }
 function leftMove(e){
     if(e.key === "ArrowLeft"){
         leftRifgtMove += 20;
         player.style.right = `${leftRifgtMove}px`
-        console.log(leftRifgtMove)
+
+    }
+}
+function upMove(e){
+    if(e.key === "ArrowUp"){
+    upDownMove += 20;
+    player.style.bottom = `${upDownMove}px`;
+    console.log("moveup")
+    }
+}
+function downMove(e){
+    if(e.key === "ArrowDown"){
+    upDownMove -= 20;
+    player.style.bottom = `${upDownMove}px`;
+    console.log("movedown")
     }
 }
