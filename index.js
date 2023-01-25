00738f52f2d04815e7dcaa0c7d3d07113c1a7c9c
sndlmncwango <sndlmncwango@gmail.com>
2023-01-25 20:32:45 +0200

    creat left and right button functions to move the div

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 const player = document.querySelector("#player");
+let leftRifgtMove = -580;
+let upDownMove = 0;
+window.addEventListener("keydown",(e)=>{
+    rightMove(e);
+    leftMove(e);
+})
+
+function rightMove(e){
+    if(e.key === "ArrowRight"){
+        leftRifgtMove -= 20;
+        player.style.right = `${leftRifgtMove}px`
+        console.log(leftRifgtMove)
+    }
+}
+function leftMove(e){
+    if(e.key === "ArrowLeft"){
+        leftRifgtMove += 20;
+        player.style.right = `${leftRifgtMove}px`
+        console.log(leftRifgtMove)
+    }
+}
