d9a09d7be63e1a0d59822053f49864ad9c961b66
sndlmncwango <sndlmncwango@gmail.com>
2023-01-28 16:34:48 +0200

    added animation to mave the enermy to attack

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const player = document.querySelector("#player");
+const container = document.querySelector("#container");
 let leftRifgtMove = 0;
 let upDownMove = 0;
 window.addEventListener("keydown",(e)=>{
@@ -20,16 +21,29 @@ function leftMove(e){
         player.style.right = `${leftRifgtMove}px`
     }
 }
-function upMove(e){
-    if(e.key === "ArrowUp" && upDownMove < 0){
-    upDownMove += 20;
-    player.style.bottom = `${upDownMove}px`;
+// function upMove(e){
+//     if(e.key === "ArrowUp" && upDownMove < 0){
+//     upDownMove += 20;
+//     player.style.bottom = `${upDownMove}px`;
 
+//     }
+// }
+// function downMove(e){
+//     if(e.key === "ArrowDown" && upDownMove > -600){
+//     upDownMove -= 20;
+//     player.style.bottom = `${upDownMove}px`;
+//     }
+// }
+//random enermies
+function createEnermy(){
+    for(let i=0;i<4;i++){
+    // const topRandom = Math.floor(Math.random()*300);
+    const leftRandom = Math.floor(Math.random()*550);
+    enermy = document.createElement("div");
+    enermy.setAttribute("class", "enermy");
+    container.appendChild(enermy)
+    enermy.style.left = `${leftRandom}px`;
+    console.log(leftRandom)
     }
 }
-function downMove(e){
-    if(e.key === "ArrowDown" && upDownMove > -600){
-    upDownMove -= 20;
-    player.style.bottom = `${upDownMove}px`;
-    }
-}
+createEnermy();
